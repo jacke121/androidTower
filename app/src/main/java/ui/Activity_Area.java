3f9f439c6f9de18b7c546623ed0aabe6535a117a
jacke121 <jacke121@126.com>
2016-03-04 21:44:32 +0800

    0303

--- app/src/main/java/ui/Activity_Area.java
+++ app/src/main/java/ui/Activity_Area.java
@@ -28,229 +28,136 @@ import com.lbg.yan01.MyApplication;
 import com.lbg.yan01.R;
 
 import webClient.DialogFactory;
+
 public class Activity_Area extends Activity implements OnClickListener {
-	 public static EditText txt_qubian, txt_quxian, txt_gongbian, txt_danwei;
-	 Button btn_save;
-	 ImageView title_btn_sequence;
-	 String msg=null;
-		public static Dialog mdlg;
-		public static String name, pwd, rpwd, registerMessage, uuid, uuid_psw,message;
-		public static Activity_Area activityArea;
-		public String msg_show ;
-	SqlHelper helper;
-	AreasDao areasDao;
-	    @SuppressLint("NewApi")
-		@Override
-	    public void onCreate(Bundle savedInstanceState) {
-	    	  super.onCreate(savedInstanceState);
-	    	  requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
-			MyApplication myApplication = (MyApplication) getApplication();
-			 helper=myApplication.getSqlHelper();
-			areasDao = new AreasDao(helper);
-			// 查询数据库
+    public static EditText txt_qubian, txt_quxian, txt_gongbian, txt_danwei;
+    Button btn_save;
+    ImageView title_btn_sequence;
+    String msg = null;
+    public static Dialog mdlg;
+    public String msg_show;
+    SqlHelper helper;
+    AreasDao areasDao;
 
-	    	///在Android2.2以后必须添加以下代码
-			//本应用采用的Android4.0
-			//设置线程的策略
-	    	StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()  
-	    	.detectDiskReads()  
-	    	.detectDiskWrites()  
-	    	.detectNetwork()   // or .detectAll() for all detectable problems  
-	    	.penaltyLog()  
-	    	.build());  
-	    	StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()  
-	    	.detectLeakedSqlLiteObjects()  
-	    	.detectLeakedClosableObjects()  
-	    	.penaltyLog()  
-	    	.penaltyDeath()  
-	    	.build());  
-	    	activityArea = this;
-	    	setContentView(R.layout.area_detail);
-	        //设置初始化视图
-	        initView();
-	        //设置事件监听器方法
-	        setListener();
-	      
-        }
-	    @SuppressLint("HandlerLeak")
-	 	public  Handler mMsgReciver = new Handler() {
-	 		@Override
-	 		public void handleMessage(Message msg) {
-	 			switch (msg.what) {
-	 			case 0:
-	 				if (mdlg != null) {
-	 					mdlg.dismiss();
-	 					mdlg = null;
-	 					showregisterMessage("注册失败重新注册");
-	 				}
-	 				break;
-	 			case 12:
-	 				if (mdlg != null) {
-	 					mdlg.dismiss();
-	 					new Builder(Activity_Area.this)
-	 							.setTitle("温馨提示")
-	 							.setMessage("网络超时了!")
-	 							.setPositiveButton("确定",
-	 									new DialogInterface.OnClickListener() {
-	 										public void onClick(
-	 												DialogInterface dialog,
-	 												int which) {
-	 											return;
-	 										}
-	 									}).create().show();
-	 				}
-	 				break;
-	 			case 13:
-	 				if (mdlg != null) {
-	 					mdlg.dismiss();
-	 					mdlg = null;
-	 					
-	 					if (msg_show!=null&&msg_show.equals("恭喜您注册成功")) {
-	 						//用户存储
-	 					
-	 						showregisterMessage(msg_show);
-	 					}else if(msg_show.equals("用户名已存在")){
-	 						showregisterMessage("用户名已存在,请重新输入！");
-	 					}else {
-	 						showregisterMessage("注册失败！");
-	 					}
-	 				}
-	 				break;
-	 			}
-	 		}
-	 	};
-	 	public void showregisterMessage(final String msg) {
-			// 创建提示框提醒是否登录成功
-			Builder builder = new Builder(Activity_Area.this);
-			builder.setTitle("提示")
-			.setMessage(msg)
-					.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							if(msg.contains("成功")){
-								//进入首次设置界面
-								Intent intent = new Intent();
-								intent.setClass(Activity_Area.this, IndexActivity.class);
-								startActivity(intent);
-							}else {
-								//进入首次设置界面
-								Intent intent = new Intent();
-								intent.setClass(Activity_Area.this, Activity_Area.class);
-								startActivity(intent);
-							}
-							dialog.dismiss();
-						}
-					}).create().show();
-		}
-		
-        /**
-         * 创建初始化视图的方法
-         */
-		private void initView() {
-			txt_qubian = (EditText) findViewById(R.id.txt_qubian);
-	    	txt_quxian = (EditText) findViewById(R.id.txt_quxian);
-	    	txt_gongbian = (EditText) findViewById(R.id.txt_gongbian);
-			txt_danwei= (EditText) findViewById(R.id.txt_danwei);
-	    	btn_save = (Button) findViewById(R.id.btn_save);
-	    	Button register_btnCancel=(Button)findViewById(R.id.register_btnCancel);
-	    	register_btnCancel.setOnClickListener(this);
-	    	title_btn_sequence=(ImageView)findViewById(R.id.title_btn_sequence);
-	    	title_btn_sequence.setOnClickListener(this);
-		}
-
-		/**
-		 * 设置事件的监听器的方法
-		 */
-		private void setListener() {
-		
-			btn_save.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					name = txt_qubian.getText().toString();
-					pwd = txt_quxian.getText().toString();
-					rpwd = txt_gongbian.getText().toString();
-//					registerRemoteService(name,pwd);
-					if (name.equals("") || pwd.equals("") || rpwd.equals("")) {
-						showMsg("信息不能为空!");
-					} else {
-						try {
-
-							mdlg = DialogFactory.createLoadingDialog(
-									Activity_Area.this, "注册中，请稍候……");
-							mdlg.getWindow().setType(
-									(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT));
-							mdlg.show();
-							new Thread() {
-								public void run() {
-									try {
-										// 原来20s
-										Thread.sleep(20000);
-									} catch (Exception e) {
-									}
-									// 休眠10s之后，若数据仍在加载，则令加载窗体消失
-									mMsgReciver.sendEmptyMessage(12);// 超时处理
-								}
-							}.start();
-							new Thread() {
-								public void run() {
-									try {
-										msg_show = registerRemoteService(name, pwd, rpwd);
-										mMsgReciver.sendEmptyMessage(13);//反馈注册消息给用户
-									} catch (Exception e) {
-									}
-								}
-							}.start();
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			});
-		}
-		public void showMsg(String msg) {
-			new Builder(Activity_Area.this).setTitle("温馨提示")
-					.setMessage(msg + "!")
-					.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							return;
-						}
-					}).create().show();
-		}
-    /**
-     * 获取Struts2 Http 登录的请求信息
-     * @param  userName
-     * @param  password
-    * @return 
-     */
-    public String registerRemoteService(String userName,String password,String repassword){
-    	    Map<String, String> params=new LinkedHashMap<String, String>();
-    	    params.put("userName", txt_qubian.getText().toString());
-    	    params.put("password", txt_quxian.getText().toString());
-    	   params.put("repassword", txt_gongbian.getText().toString());
-    	    return msg;
+    @SuppressLint("NewApi")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
+        MyApplication myApplication = (MyApplication) getApplication();
+        helper = myApplication.getSqlHelper();
+        areasDao = new AreasDao(helper);
+        // 查询数据库
+        setContentView(R.layout.area_detail);
+        //设置初始化视图
+        initView();
     }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-			case R.id.btn_save:
-				Areas areas=new Areas();
-				areas.city="";
-				areas.area="";
-				areasDao.insert(areas);
-				break;
-			case R.id.register_btnCancel:
-				Intent intent=new Intent(Activity_Area.this, IndexActivity.class);
-				startActivity(intent);
-				Activity_Area.this.finish();
-				break;
-			case R.id.title_btn_sequence:
-				Activity_Area.this.finish();
-				break;
-			default:
-				break;
-		}
-	}
+    @SuppressLint("HandlerLeak")
+    public Handler mMsgReciver = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    if (mdlg != null) {
+                        mdlg.dismiss();
+                        mdlg = null;
+                        showregisterMessage("注册失败重新注册");
+                    }
+                    break;
+                case 12:
+              break;
+
+            }
+        }
+    };
+
+    public void showregisterMessage(final String msg) {
+        // 创建提示框提醒是否登录成功
+        Builder builder = new Builder(Activity_Area.this);
+        builder.setTitle("提示")
+                .setMessage(msg)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (msg.contains("成功")) {
+                            //进入首次设置界面
+                            Intent intent = new Intent();
+                            intent.setClass(Activity_Area.this, IndexActivity.class);
+                            startActivity(intent);
+                        } else {
+                            //进入首次设置界面
+                            Intent intent = new Intent();
+                            intent.setClass(Activity_Area.this, Activity_Area.class);
+                            startActivity(intent);
+                        }
+                        dialog.dismiss();
+                    }
+                }).create().show();
+    }
+
+    /**
+     * 创建初始化视图的方法
+     */
+    private void initView() {
+        txt_qubian = (EditText) findViewById(R.id.txt_qubian);
+        txt_quxian = (EditText) findViewById(R.id.txt_quxian);
+        txt_gongbian = (EditText) findViewById(R.id.txt_gongbian);
+        txt_danwei = (EditText) findViewById(R.id.txt_danwei);
+        btn_save = (Button) findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(this);
+        Button register_btnCancel = (Button) findViewById(R.id.register_btnCancel);
+        register_btnCancel.setOnClickListener(this);
+        title_btn_sequence = (ImageView) findViewById(R.id.title_btn_sequence);
+        title_btn_sequence.setOnClickListener(this);
+    }
+
+    public void showMsg(String msg) {
+        new Builder(Activity_Area.this).setTitle("温馨提示")
+                .setMessage(msg + "!")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                }).create().show();
+    }
+
+    public String registerRemoteService(String userName, String password, String repassword) {
+        Map<String, String> params = new LinkedHashMap<String, String>();
+        params.put("userName", txt_qubian.getText().toString());
+        params.put("password", txt_quxian.getText().toString());
+        params.put("repassword", txt_gongbian.getText().toString());
+        return msg;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_save:
+               String qubian = txt_qubian.getText().toString();
+                String  quxian = txt_quxian.getText().toString();
+                String gongbian = txt_gongbian.getText().toString();
+//					registerRemoteService(name,pwd);
+                if (qubian.equals("") || quxian.equals("") || gongbian.equals("")) {
+                    showMsg("信息不能为空!");
+                } else {
+                    Areas areas = new Areas();
+                    areas.city = "";
+                    areas.area = "";
+                    areasDao.insert(areas);
+                }
+
+                break;
+            case R.id.btnCancel:
+                Intent intent = new Intent(Activity_Area.this, IndexActivity.class);
+                startActivity(intent);
+                Activity_Area.this.finish();
+                break;
+            case R.id.title_btn_sequence:
+                Activity_Area.this.finish();
+                break;
+            default:
+                break;
+        }
+    }
 }
--- app/src/main/java/ui/Activity_AreaList.java
+++ app/src/main/java/ui/Activity_AreaList.java
@@ -26,36 +26,29 @@ import android.widget.TextView;
 import com.baseDao.Areas;
 import com.baseDao.AreasDao;
 import com.baseDao.SqlHelper;
-import com.google.gson.Gson;
 import com.lbg.yan01.MyApplication;
 import com.lbg.yan01.R;
 import com.listview.CHScrollView;
-//import com.objs.AreaInfo;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Activity_AreaList extends Activity {
-	Button add_new;
+public class Activity_AreaList extends Activity implements OnClickListener {
+	Button btn_add;
 	ImageButton b_back;
-	SharedPreferences sp;
 	private int selectItem = -1;
 	int[] headIds = new int[] { R.id.txt_1201, R.id.txt_1202, R.id.txt_1203,
 			R.id.txt_1204, R.id.txt_1205 };
-	String[] headers = new String[] { "序号", "城市", "台区名称","已完成", "操作" };
+	String[] headers = new String[] { "序号", "城市", "台区名称","状态", "操作" };
 	// 方便测试，直接写的public
-	Button sence_check, repair_record;
 	DataAdapter adapter;
 	public HorizontalScrollView mTouchView;
 	// 装入所有的HScrollView
 	protected List<CHScrollView> mHScrollViews = new ArrayList<CHScrollView>();
-//	AreaInfo repairOrder;
 	private LayoutInflater mInflater;
 	SparseArray<Areas> areaslist;
 	LinearLayout repairhead;
 	private ListView mListView;
-	Gson gson;
-	String message;
 	SqlHelper helper;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -72,84 +65,10 @@ public class Activity_AreaList extends Activity {
 		if (areaslist == null || areaslist.size() == 0) {
 		}
 
+        btn_add= (Button) findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(this);
 		TextView title_text = (TextView) findViewById(R.id.title_text);
 		title_text.setText("台区列表");
-		ImageButton b_back = (ImageButton) findViewById(R.id.b_back);
-		b_back.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				finish();
-			}
-		});
-		sence_check = (Button) findViewById(R.id.sence_check);
-		repair_record = (Button) findViewById(R.id.repair_regis);
-		sence_check.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				if (selectItem == -1) {
-					new AlertDialog.Builder(Activity_AreaList.this)
-							.setTitle("温馨提示")
-							.setMessage("请选择一行!")
-							.setPositiveButton("确定",
-									new DialogInterface.OnClickListener() {
-										public void onClick(
-												DialogInterface dialog,
-												int which) {
-											return;
-										}
-									}).create().show();
-					return;
-				}
-				String cityname = areaslist.get(selectItem).city;
-				String content =areaslist.get(selectItem).area;
-				Bundle bundle = new Bundle();
-				/* 字符、字符串、布尔、字节数组、浮点数等等，都可以传 */
-				bundle.putString("content", content);
-			}
-		});
-
-		repair_record.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				if (selectItem == -1) {
-					new AlertDialog.Builder(Activity_AreaList.this)
-							.setTitle("温馨提示")
-							.setMessage("请选择一行!")
-							.setPositiveButton("确定",
-									new DialogInterface.OnClickListener() {
-
-										public void onClick(
-												DialogInterface dialog,
-												int which) {
-											return;
-										}
-									}).create().show();
-					return;
-				}
-				Intent intent = null;
-				if (areaslist.size() > selectItem) {
-					String cityname =areaslist.get(selectItem).city;
-
-					Bundle bundle = new Bundle();
-
-					String content =areaslist.get(selectItem).area;
-					/* 字符、字符串、布尔、字节数组、浮点数等等，都可以传 */
-					bundle.putString("maintainNum", cityname);
-					bundle.putString("content", content);
-
-					/* 把bundle对象assign给Intent */
-					intent.putExtras(bundle);
-					// startActivity(intent);
-					startActivityForResult(intent, 1);
-				}
-
-			}
-		});
 
 		for (int i = 0; i < headers.length; i++) {
 			TextView tmpView = ((TextView) findViewById(headIds[i]));
@@ -158,23 +77,23 @@ public class Activity_AreaList extends Activity {
 		repairhead = (LinearLayout) findViewById(R.id.line_head);
 		CHScrollView headerScroll = (CHScrollView) findViewById(R.id.repairheader);
 		headerScroll.registerScrollChange(new CHScrollView.ScrollChange() {
-			@Override
-			public void onScrollChanged(int l, int t, int oldl, int oldt) {
-				// TODO Auto-generated method stub
-				for (CHScrollView mscrollView : mHScrollViews) {
-					// 防止重复滑动
-					if (mTouchView != mscrollView)
-						mscrollView.smoothScrollTo(l, t);
-				}
-			}
-		});
+            @Override
+            public void onScrollChanged(int l, int t, int oldl, int oldt) {
+                // TODO Auto-generated method stub
+                for (CHScrollView mscrollView : mHScrollViews) {
+                    // 防止重复滑动
+                    if (mTouchView != mscrollView)
+                        mscrollView.smoothScrollTo(l, t);
+                }
+            }
+        });
 		// 添加头滑动事件
 		mHScrollViews.add(headerScroll);
 		mListView = (ListView) findViewById(R.id.scroll_list);
 		if (areaslist == null) {
 			// mListView.setVisibility(View.GONE);
 			areaslist = new SparseArray<Areas>();
-			 for (int i = 0; i < 30; i++) {
+			 for (int i = 0; i < 15; i++) {
 				 Areas rows = new Areas();
 			 rows.city = "" + i;
 			 rows.area = i + "content";
@@ -183,24 +102,31 @@ public class Activity_AreaList extends Activity {
 			 }
 		}
 		adapter = new DataAdapter();
-
 		mListView.setAdapter(adapter);
-//		downData();
 		mInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
-		// mListView = (ListView) this.findViewById(R.id.repair_listview);
 		b_back = (ImageButton) findViewById(R.id.b_back);
-		b_back.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				// 跳转到主界面
-				finish();
-			}
-		});
+		b_back.setOnClickListener(this);
 
 	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_add:
+                Intent intent=new Intent(Activity_AreaList.this, Activity_Area.class);
+                startActivity(intent);
+                Activity_AreaList.this.finish();
+                break;
+            case R.id.b_back:
+                finish();
+                break;
+            case R.id.title_btn_sequence:
+                Activity_AreaList.this.finish();
+                break;
+            default:
+                break;
+        }
+    }
 	Dialog mdlg;
 	public void onScrollChanged (int l, int t, int oldl, int oldt) {
 		for (CHScrollView scrollView : mHScrollViews) {
@@ -237,7 +163,9 @@ public class Activity_AreaList extends Activity {
 
 	}
 
-	private class DataAdapter extends BaseAdapter {
+
+
+    private class DataAdapter extends BaseAdapter {
 
 		@Override
 		public int getCount() {
@@ -262,17 +190,10 @@ public class Activity_AreaList extends Activity {
 					Areas tmpRepairOrder =areaslist.get(position);
 					if (tmpRepairOrder.areastatus.equals("1")) {
 						// holder.orderState.setText("发布");
-						repair_record.setEnabled(false);
-						sence_check.setEnabled(true);
 					} else if (tmpRepairOrder.areastatus.equals("2")
 							|| tmpRepairOrder.areastatus.equals("4")) {
 						// holder.orderState.setText("维修申请  ");
-						repair_record.setEnabled(false);
-						sence_check.setEnabled(false);
 					} else if (tmpRepairOrder.areastatus.equals("3")) {
-
-						repair_record.setEnabled(true);
-						sence_check.setEnabled(false);
 						// holder.orderState.setText("审批通过");
 					}
 					notifyDataSetChanged();// 提醒数据已经变动
@@ -285,9 +206,12 @@ public class Activity_AreaList extends Activity {
 			} else {
 				convertView.setBackgroundColor(Color.TRANSPARENT);
 			}
-
 			final ViewHolder holder = new ViewHolder();
 			// holder.maintainNum.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+
+			holder.rownember = (TextView) convertView
+					.findViewById(R.id.rownember);
 			holder.areanmae = (TextView) convertView
 					.findViewById(R.id.areaname);
 
@@ -307,11 +231,10 @@ public class Activity_AreaList extends Activity {
 			} else if (tmpAreaInto.areastatus.equals("4")) {
 				holder.orderState.setText("维修完成");
 			}
+			holder.rownember.setText(position+1+"");
 			holder.cityname.setText(tmpAreaInto.city);
 			// holder.maintainNum.setText(Html.fromHtml("<u>"+tmpRepairOrder.maintainNum+"</u>"));
 			holder.areanmae.setText(tmpAreaInto.area);
-//			holder.content.setText(tmpAreaInto.content);
-
 			convertView.setTag(holder);
 			CHScrollView ascrollView = (CHScrollView) convertView
 					.findViewById(R.id.item_scroll);
@@ -322,7 +245,6 @@ public class Activity_AreaList extends Activity {
 					// TODO Auto-generated method stub
 					for (CHScrollView mscrollView : mHScrollViews) {
 						// 防止重复滑动
-//						if (mTouchView != mscrollView)
 							mscrollView.smoothScrollTo(l, t);
 					}
 				}
@@ -358,6 +280,8 @@ public class Activity_AreaList extends Activity {
 		}
 	}
 	private class ViewHolder {
+		TextView rownember;
+
 		TextView cityname;
 		TextView areanmae;
 		TextView content;
